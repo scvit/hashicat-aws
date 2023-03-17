e89ad220d62f919a4b0dbc66f820cb704f8bfdb7
scvit <myscvit@gmail.com>
2023-03-17 06:49:44 +0000

    Added s3 bucket module

--- main.tf
+++ main.tf
@@ -126,12 +126,15 @@ resource "aws_instance" "hashicat" {
   subnet_id                   = aws_subnet.hashicat.id
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
   
+  mandatory_tags = [
+  "Department",
+  "Billable",
+]
 
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    "Department"
-    "Billable"
+    
   }
 }
 
