bffb77e73397b8e2b519bd6cf1dbde0b45a200d3
scvit <myscvit@gmail.com>
2023-03-17 07:27:38 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,4 @@
-module "s3-bucket-1" {
-  source  = "app.terraform.io/mwjochip/s3-bucket-1/aws"
+module "s3-bucket" {
+  source  = "app.terraform.io/mwjochip/s3-bucket/aws"
   version = "2.8.0"
-  
 }
